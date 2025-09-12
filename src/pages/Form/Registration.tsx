9cba54d840a655ade36be4d9930439fac6e32e63
Adarshk33 <adarsh.sinha33@gmail.com>
2025-09-12 13:44:04 +0530

    worked on api

--- src/pages/Form/Registration.tsx
+++ src/pages/Form/Registration.tsx
@@ -7,7 +7,7 @@ import { handleInputChange } from "../../lib/validationUtils.ts";
 import { useTranslation } from "react-i18next";
 
 import down from "../../assets/images/select_down.svg";
-import styles from "../regis/registration.module.scss";
+import styles from "../Registration/registration.module.scss";
 import API from "../../api/index.ts";
 import { ERROR_IDS } from "../../api/utils.ts";
 
