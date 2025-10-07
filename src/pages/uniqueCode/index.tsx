97883dcaafcf41b5d573666ad35b80a0ccd2678a
Adarshk33 <adarsh.sinha33@gmail.com>
2025-10-07 20:05:29 +0530

    first commit

--- src/pages/uniqueCode/index.tsx
+++ src/pages/uniqueCode/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styles from "./uniquecode.module.scss";
 import { IMAGES } from "../../lib/assets";
 import CommonImage from "../../components/common/Image";
