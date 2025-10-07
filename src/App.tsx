86118e6d11dae023dbce58b422d2c9c019590e93
Adarshk33 <adarsh.sinha33@gmail.com>
2025-10-07 16:35:38 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,24 @@
 import "./App.scss";
 
-import { Route, Routes, useLocation,  } from "react-router-dom";
-import { useEffect, Suspense, lazy, useLayoutEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { useEffect, useLayoutEffect } from "react";
 import { useGlobalLoaderContext } from "./helpers/GlobalLoader";
 import API from "./api";
 import { ROUTES } from "./lib/consts";
 import PrivateRoute from "./helpers/PrivateRoute";
-const GlobalSuspenseLoader = lazy(() => import("./helpers/UiLoader"));
 
-// import PrivateRoute from "./helpers/PrivateRoute";
-
-const Home = lazy(() => import("./pages/Home"));
-
-const Vote = lazy(() => import("./pages/Menu/menu"));
-const CYC = lazy(() => import("./pages/Cyc/CYC"));
-const ThankYou = lazy(() => import("./pages/ThanyouVote/Thankyou"));
-const Registration = lazy(() => import("./pages/Registration/Registration"));
-const OtpVerification = lazy(
-  () => import("./pages/VerificationOtp/VerificationOtp"),
-);
-const CashBack = lazy(() => import("./pages/CashBackMethod/cashBack"));
-const ThankYouParticipation = lazy(
-  () => import("./pages/ThankYouParticipation/ThankYouParticipation"),
-);
+// Normal imports
+import Home from "./pages/Home";
+import Vote from "./pages/Menu/menu";
+import CYC from "./pages/Cyc/CYC";
+import ThankYou from "./pages/ThanyouVote/Thankyou";
+import Registration from "./pages/Registration/Registration";
+import OtpVerification from "./pages/VerificationOtp/VerificationOtp";
+import CashBack from "./pages/CashBackMethod/cashBack";
+import ThankYouParticipation from "./pages/ThankYouParticipation/ThankYouParticipation";
 
 function App() {
   const location = useLocation();
-  //  const navigate = useNavigate();
   const { showLoader, hideLoader } = useGlobalLoaderContext();
 
   useEffect(() => {
@@ -38,50 +30,38 @@ function App() {
     window.addEventListener("offline", () => {
       API.setIsOnline(false);
     });
-
-  // navigate("/", { replace: true });
-
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [showLoader, hideLoader]);
 
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
-    // console.log("scroll to top on route change:");
   }, [location.pathname]);
 
   return (
     <div className="App">
-      <Suspense fallback={<GlobalSuspenseLoader />}>
-        <Routes>
-          <Route path={ROUTES.HOME} element={<Home />} />
-
-          <Route path={ROUTES.VOTE} element={<Vote />} />
-          <Route path={ROUTES.CYC} element={<CYC />} />
-          <Route path={ROUTES.ThankYou} element={<ThankYou />} />
-          <Route path={ROUTES.REGISTRATION} element={<Registration />} />
-          <Route path={ROUTES.VERIFYOTP} element={<OtpVerification />} />
-          <Route path={ROUTES.CASHBACK} element={
-             <PrivateRoute>
-   <CashBack />
-             </PrivateRoute>
-         
-            } />
-          <Route
-            path={ROUTES.ThankYouParticipation}
-            element={
+      <Routes>
+        <Route path={ROUTES.HOME} element={<Home />} />
+        <Route path={ROUTES.VOTE} element={<Vote />} />
+        <Route path={ROUTES.CYC} element={<CYC />} />
+        <Route path={ROUTES.ThankYou} element={<ThankYou />} />
+        <Route path={ROUTES.REGISTRATION} element={<Registration />} />
+        <Route path={ROUTES.VERIFYOTP} element={<OtpVerification />} />
+        <Route
+          path={ROUTES.CASHBACK}
+          element={
             <PrivateRoute>
-   <ThankYouParticipation />
+              <CashBack />
             </PrivateRoute>
-
-          
-         
-             
-            
-            }
-          />
-        </Routes>
-      </Suspense>
+          }
+        />
+        <Route
+          path={ROUTES.ThankYouParticipation}
+          element={
+            <PrivateRoute>
+              <ThankYouParticipation />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
     </div>
   );
 }
