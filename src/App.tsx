f0c704e54186114fe751ce5c2d011eccb21eb3b7
Adarshk33 <adarsh.sinha33@gmail.com>
2025-11-04 15:04:02 +0530

    worked on new client changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useEffect, useLayoutEffect } from "react";
 import { useGlobalLoaderContext } from "./helpers/GlobalLoader";
 import API from "./api";
 import { ROUTES } from "./lib/consts";
-// import PrivateRoute from "./helpers/PrivateRoute";
+ import PrivateRoute from "./helpers/PrivateRoute";
 
 // Normal imports
 import Home from "./pages/Home";
@@ -18,12 +18,12 @@ import { logoutUser } from "./lib/utils";
 import { setUserKey } from "./store/slices/authSlice";
 import { store } from "./store/store";
 
-// import { useNavigate } from "react-router-dom";
+ import { useNavigate } from "react-router-dom";
 import UserRegister from "./pages/UserReg";
 
 function App() {
   const location = useLocation();
-    //const navigate = useNavigate();
+    const navigate = useNavigate();
   const { showLoader, hideLoader } = useGlobalLoaderContext();
 
   useEffect(() => {
@@ -37,7 +37,7 @@ function App() {
 
         await store.dispatch(setUserKey(userResponse));
 
-            //  navigate(ROUTES.HOME + window.location.search);
+              navigate(ROUTES.HOME + window.location.search);
       } catch (err) {
         console.log("error", err);
       }
@@ -70,17 +70,18 @@ function App() {
           <Route
           path={ROUTES.CASHBACK}
           element={
-             
+               <PrivateRoute>
   <CashBack />
+                </PrivateRoute>
               
           }
         />
         <Route
           path={ROUTES.THANK_YOU_PARTICIPATION}
           element={
-              //  <PrivateRoute>
+                <PrivateRoute>
    <ThankYouParticipation />
-              //  </PrivateRoute>
+                </PrivateRoute>
           
           }
         />
