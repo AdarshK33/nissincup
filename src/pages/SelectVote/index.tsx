96c1899d8bc77f1e15a1d34d4162f8f8c2cb0026
Adarshk33 <adarsh.sinha33@gmail.com>
2025-11-10 16:21:35 +0530

    worked on animation

--- src/pages/SelectVote/index.tsx
+++ src/pages/SelectVote/index.tsx
@@ -1,6 +1,6 @@
 import styles from "./selectVote.module.scss";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import { useNavigate } from "react-router-dom";
 import CommonBase from "../../components/common/CommonBase";
